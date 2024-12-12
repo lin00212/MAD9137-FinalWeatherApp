57c34543ce24e25c4e7ed905bcd402c12ad97c2d
Eason Lin <lin00212@algonquinlive.com>
2024-12-12 10:22:21 -0500

    Initial Commit

---
+++ RichWeather_App/RichWeather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RichWeather_AppApp.swift
+//  RichWeather_App
+//
+//  Created by Eason Lin on 12/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct RichWeather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
